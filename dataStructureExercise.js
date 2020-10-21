86391371e8cddd76d38e22c2cdd463ead69eb4f7
Travis Whitworth <traviswhitworth@BCs-MacBook-Pro-5.local>
2020-10-21 12:28:00 -0500

    Completed Exercise

--- dataStructureExercise.js
+++ dataStructureExercise.js
@@ -44,19 +44,31 @@ const droids = {
     assassin: "IG-88"
 };
 console.log(droids);
-// // 3b
+// 3b
 console.log(droids ["astromech"]);
 
-// // 3c
+// 3c
 droids.protocol
-console.log(droids.protocol);
+console.log(droids);
 
-// // 3d
+// 3d
 droids.assassin = "IG-11";
-console.log(droid.assassin);
+console.log(droids);
 
-// // // BONUS
-// // // 4
-console.log(starWarsVillains);
-console.log(starWarsVillains[0]);
+// BONUS
+// 4
 console.log(starWarsVillains[0][6]);
+
+// 5
+console.log(sithLords.slice(-2, -1));
+
+// 6a
+const starWarsMovies = [
+{episodeOne: "The Phantom Meanace", episodeTwo: "Attack of the Clones", episodeThree: "Revenge of the Sith"},
+{episodeFour: "A New Hope", episodeFive: "The Empire Strikes Back", episodeSix: "Return of the Jedi"},
+{episodeSeven: "The Force Awakens", episodeEight: "The Last Jedi", episodeNine: "The Rise of Skywalker"}
+];
+
+// 6b
+starWarsMovies.splice(1, 0, "Solo", "Rouge One");
+console.log(starWarsMovies);
